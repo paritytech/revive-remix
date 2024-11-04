0e12bd4a14f3862226c90e8f0785f058c842e43d
Sebastian Miasojed <sebastian.miasojed@parity.io>
2024-11-04 13:34:43 +0100

    Update RPC node address of Westend Assethub chain

--- apps/remix-ide/src/app/providers/injected-westend-provider.tsx
+++ apps/remix-ide/src/app/providers/injected-westend-provider.tsx
@@ -16,7 +16,7 @@ export class InjectedWestendTestnetProvider extends InjectedCustomProvider {
     super(profile,
       'Westend Testnet',
       '0x190f1b44',
-      ['https://eth-rpc.parity-testnet.parity.io'],
+      ['https://westend-asset-hub-eth-rpc.polkadot.io'],
       {
         "name": "Testnet token WND",
         "symbol": "WND",
