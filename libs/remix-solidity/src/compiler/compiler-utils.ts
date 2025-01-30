8945c4895c526cbd3ffb6bf3984ca31bdef7ba66
Sebastian Miasojed <sebastian.miasojed@parity.io>
2025-01-30 16:29:40 +0100

    Add local compilers

--- libs/remix-solidity/src/compiler/compiler-utils.ts
+++ libs/remix-solidity/src/compiler/compiler-utils.ts
@@ -1,7 +1,7 @@
 import * as semver from 'semver'
 /* global Worker */
 
-export const baseURLBin = 'https://remix-backend.polkadot.io/wasm'
+export const baseURLBin = 'assets/js'
 export const baseURLWasm = baseURLBin
 
 export const pathToURL = {}
