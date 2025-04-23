987ecba8874bfcbc9590041731b7e421cbad2763
Sebastian Miasojed <s.miasojed@gmail.com>
2025-04-23 12:38:07 +0200

    Add WalletConnect support (#20)

--- apps/remix-ide/src/remixAppManager.js
+++ apps/remix-ide/src/remixAppManager.js
@@ -91,8 +91,7 @@ let requiredModules = [ // services + layout views + system views
 // dependentModules shouldn't be manually activated (e.g hardhat is activated by remixd)
 const dependentModules = ['foundry', 'hardhat', 'truffle', 'slither']
 
-// const loadLocalPlugins = ['doc-gen', 'doc-viewer', 'etherscan', 'vyper', 'solhint', 'walletconnect', 'circuit-compiler', 'learneth', 'quick-dapp']
-const loadLocalPlugins = ['solhint', 'doc-gen', 'doc-viewer', 'quick-dapp']
+const loadLocalPlugins = ['solhint', 'doc-gen', 'doc-viewer', 'quick-dapp', 'walletconnect']
 
 const sensitiveCalls = {
   fileManager: ['writeFile', 'copyFile', 'rename', 'copyDir'],
--- apps/walletconnect/src/services/constant.ts
+++ apps/walletconnect/src/services/constant.ts
@@ -3,6 +3,6 @@ export const PROJECT_ID = WALLET_CONNECT_PROJECT_ID
 export const METADATA = {
   name: 'Remix IDE',
   description: 'The Native IDE for Web3 Development.',
-  url: 'https://remix.ethereum.org/',
-  icons: ['https://remix.ethereum.org/favicon.ico'],
+  url: 'https://remix.polkadot.io/',
+  icons: ['https://remix.polkadot.io/favicon.ico'],
 }
